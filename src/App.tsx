c770310422e283300883834d935144f19c101153
erikl <erilundin@gmail.com>
2024-05-27 11:14:18 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">test</header>
+    <div>
+      <header>test</header>
     </div>
   );
 }
